17734f417c4d982679b6a4f04838e8980e2fe356
wangxingyu <wxy18115116156@163.com>
2024-09-24 14:26:13 +0800

    Revert "thymleaf学习"
    This reverts commit afc6143470d1c03e6b594b2972d9f185aee4ad53.

--- spring-boot-quickpractice/src/main/java/top/wangxingyu/springboot/quickstart/service/CustomerService.java
+++ spring-boot-quickpractice/src/main/java/top/wangxingyu/springboot/quickstart/service/CustomerService.java
@@ -1,29 +1,4 @@
 package top.wangxingyu.springboot.quickstart.service;
 
-import org.springframework.stereotype.Service;
-import top.wangxingyu.springboot.quickstart.enums.RequestType;
-
-import static org.apache.catalina.util.URLEncoder.QUERY;
-
-@Service
 public class CustomerService {
-    public String handleRequest(RequestType requestType){
-        return switch (requestType){
-            case QUERY -> handleQuery();
-            case COMPLAINT -> handleComplaint();
-            case SUGGESTION -> handleSuggestion();
-        };
-    }
-
-    private String handleQuery(){
-        return "查询请求";
-    }
-
-    private String handleComplaint(){
-        return "投诉请求";
-    }
-
-    private String handleSuggestion(){
-        return "建议请求";
-    }
 }
--- spring-boot-quickpractice/src/main/java/top/wangxingyu/springboot/quickstart/service/MeetingService.java
+++ spring-boot-quickpractice/src/main/java/top/wangxingyu/springboot/quickstart/service/MeetingService.java
@@ -1,22 +1,4 @@
 package top.wangxingyu.springboot.quickstart.service;
 
-
-import org.springframework.stereotype.Service;
-import top.wangxingyu.springboot.quickstart.entity.Meeting;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Service
 public class MeetingService {
-    private final List<Meeting> meetings=new ArrayList<>();
-
-    public boolean isRoomAvailable(Meeting newMeeting){
-        return meetings.stream()
-                .noneMatch(exitingMeeting -> exitingMeeting.isOverlapping(newMeeting));
-    }
-
-    public void addMeeting(Meeting meeting){
-        meetings.add(meeting);
-    }
 }
